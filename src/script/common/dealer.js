4c215cba42d12fd1ea594420ba8b1ed694d1ea3e
541tf88 <tangfeng1994@163.com>
2019-12-15 20:02:10 +0800

    add deliver&shuffle function

--- src/script/common/dealer.js
+++ src/script/common/dealer.js
@@ -1,12 +1,56 @@
 import { generateWholeCards } from './tool'
-function Dealer (isJokerAdded) {
-    this.isJokerAdded = isJokerAdded
-    this.stack = generateWholeCards(isJokerAdded)
-    this.public = []
+class Dealer{
+    constructor(isJokerAdded){
+
+        this.isJokerAdded = isJokerAdded;
+        this.restart();
+        // this.public = []
+    }
+    
+    restart(){
+        this.stack = generateWholeCards(this.isJokerAdded)
+        this.shuffle();
+    }
+
+    deliver(params){
+
+        let defualtParamas  = {
+            split:4,//Maybe person count
+            each:2,//Card count each one,e.g. -1 means deliver all cards in stack,2 means deliver 2 cards for each one
+        };
+        params = Object.assign({},defualtParamas,params);
+
+        let split = params.split;
+        let each = params.each;
+
+        let i=split;
+        let deliverArr = [];
+        while(--i >= 0){
+            deliverArr.push([]);
+        }
+
+        let stack = this.stack;
+        let index = stack.length;
+        let continueFlag = true;
+        let endIndex = split-1;
+        while(--index >= 0 && continueFlag){
+            let splitIndex = endIndex - (index % split);
+            deliverArr[splitIndex].push(stack.pop());
+            continueFlag = deliverArr[endIndex].length < each || each === -1;
+        }
+
+        return deliverArr;
+    }
+
+    shuffle(){
+        let copyStack = this.stack.slice(0);
+        this.stack = [];
+        let len;
+        while((len = copyStack.length)>0){
+            let index = Math.floor(Math.random()*len);
+            this.stack.push(copyStack[index]);
+            copyStack.splice(index,1);
+        }
+    }
 }
-Dealer.prototype.restart = function () {
-    this.stack = generateWholeCards(this.isJokerAdded)
-}
-Dealer.prototype.sendCards = function () {
-    this.stack = generateWholeCards(this.isJokerAdded)
-}
+

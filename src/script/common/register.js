7cc05360c50cc35f5f2bef820a14acfe62dd839f
412799755 <412799755@qq.com>
2019-12-19 14:02:24 +0800

    [add]common components register(from single to group)

--- src/script/common/register.js
+++ src/script/common/register.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
-import radio from '../../components/common/radio'
-import selects from '../../components/common/selects'
 
-Vue.component('radio', radio)
-Vue.component('selects', selects)
+const requireComponent = require.context(
+    '../../components/common',
+    false,
+    /\w+\.(vue|js)$/
+)
+requireComponent.keys().forEach(fileName => {
+    // Get component config
+    const componentConfig = requireComponent(fileName)
+
+    // Get PascalCase name of component
+    const componentName = fileName.split('/').pop().replace(/\.\w+$/, '')
+
+    // Register component globally
+    Vue.component(
+        componentName,
+        // Look for the component options on `.default`, which will
+        // exist if the component was exported with `export default`,
+        // otherwise fall back to module's root.
+        componentConfig.default || componentConfig
+    )
+})
 Vue.prototype.guid = function () {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
         function (c) {
